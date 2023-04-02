9ce8d677317e68914712b8f8b8d880e2a88cfdef
C1310849 Diogenes da Silva Oliveira <c1310849@interno.bb.com.br>
2023-04-02 16:32:01 -0300

    try: run maven at once

---
+++ src/test/java/io/github/diogenes1oliveira/hbase2/DummyTest.java
@@ -0,0 +1,13 @@
+package io.github.diogenes1oliveira.hbase2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class DummyTest {
+    @Test
+    void mathShouldWork() {
+        assertThat(2 + 2, equalTo(4));
+    }
+}
