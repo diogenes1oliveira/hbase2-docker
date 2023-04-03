360f464a0276469a099cc9f74cbfab2d22420d12
C1310849 Diogenes da Silva Oliveira <c1310849@interno.bb.com.br>
2023-04-03 05:18:23 -0300

    try: more logs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,52 +2,61 @@
 
 set -euo pipefail
 
+function log_info {
+    echo "$(date -u '+%Y-%m-%dT:%H:%M:%S.%NZ') INFO [docker-entrypoint.sh] $*" >&2
+}
+
 if [ -n "${HBASE_ENV_FILE:-}" ]; then
     if ! [ -e "$HBASE_ENV_FILE" ]; then
         while ! [ -e "$HBASE_ENV_FILE" ]; do
-            echo >&2 "INFO: env file still not present in $HBASE_ENV_FILE, trying again in 5s"
+            log_info "env file still not present in $HBASE_ENV_FILE, trying again in 5s"
             sleep 5
         done
         # make sure we've written everything in the .env
         sleep 3
     fi
 
-    echo >&2 "INFO: loading environment variables from $HBASE_ENV_FILE"
     # shellcheck disable=SC1091
     source dotenv-load "$HBASE_ENV_FILE"
+    log_info "loaded environment variables from $HBASE_ENV_FILE"
 fi
 
 
 HBASE_SITE_PATH="${HBASE_CONF_DIR:-}/hbase-site.xml"
 
 if ! [ -e "$HBASE_SITE_PATH" ] && [ -d "${HBASE_CONF_DIR:-}" ]; then
-    echo >&2 "INFO: generating hbase-site.xml from environment variables"
     hbase-xml-from-env HBASE_SITE_ > "$HBASE_SITE_PATH"
+    log_info "generated hbase-site.xml from environment variables"
 fi
 
 
 HBASE_POLICY_PATH="${HBASE_CONF_DIR:-}/hbase-policy.xml"
 
 if ! [ -e "$HBASE_POLICY_PATH" ] && [ -d "${HBASE_CONF_DIR:-}" ]; then
-    echo >&2 "INFO: generating hbase-policy.xml from environment variables"
     hbase-xml-from-env HBASE_POLICY_ > "$HBASE_POLICY_PATH"
+    log_info "generated hbase-policy.xml from environment variables"
 fi
 
 
 if [ -d /docker-entrypoint-init.d ]; then
-    echo >&2 "INFO: finding initialization scripts"
+    log_info "finding initialization scripts"
 
     while read -r script; do
-        echo >&2 "INFO: executing initialization script $script"
+        log_info "executing initialization script $script"
         # shellcheck disable=SC1090
         source "$script"
     done < <(find /docker-entrypoint-init.d -mindepth 1 -maxdepth 1 -name '*.sh' | sort )
+
+    log_info "executed all initialization scripts"
 fi
 
 if [ -n "${HBASE_PORT_MAPPINGS:-}" ]; then
-    echo >&2 "INFO: mapping ports $HBASE_PORT_MAPPINGS"
+    log_info "mapping ports $HBASE_PORT_MAPPINGS"
     tcp-map-ports "$HBASE_PORT_MAPPINGS"
+    log_info "all ports mapped"
 fi
+
+log_info "will now start HBase"
 
 if [ -n "${HBASE_RUN_AS:-}" ]; then
     exec runuser -u "$HBASE_RUN_AS" -- "$@"
--- src/test/java/io/github/diogenes1oliveira/hbase2/DummyTest.java
+++ src/test/java/io/github/diogenes1oliveira/hbase2/DummyTest.java
@@ -1,13 +1,19 @@
 package io.github.diogenes1oliveira.hbase2;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class DummyTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HBaseContainerIT.class);
+
     @Test
     void mathShouldWork() {
+        LOGGER.warn("running {}", DummyTest.class.getName());
+
         assertThat(2 + 2, equalTo(4));
     }
 }
