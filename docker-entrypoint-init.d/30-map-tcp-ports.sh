749a83066bcf2197be72a88ef9fb0aad0e9265a8
C1310849 Diogenes da Silva Oliveira <c1310849@interno.bb.com.br>
2023-07-28 16:45:16 -0300

    fix: bad splitting in 30-map-tcp-ports.sh

--- docker-entrypoint-init.d/30-map-tcp-ports.sh
+++ docker-entrypoint-init.d/30-map-tcp-ports.sh
@@ -5,12 +5,21 @@ set -euo pipefail
 # Initialization script that sets up port mappings from $HBASE_PORT_MAPPINGS
 
 if [ -n "${HBASE_PORT_MAPPINGS:-}" ]; then
-    if [ -z "${HBASE_BACKGROUND_PIDS_FILE:-}" ]; then
-        echo >&2 "ERROR: no PIDs file"
-        exit 1
-    fi
-
     (
+        HBASE_PORT_MAPPINGS="$(tr -s '[:space:],' ',' <<<"$HBASE_PORT_MAPPINGS")"
+        HBASE_PORT_MAPPINGS="${HBASE_PORT_MAPPINGS##,}"
+        HBASE_PORT_MAPPINGS="${HBASE_PORT_MAPPINGS%%,}"
+        HBASE_PORT_MAPPINGS="${HBASE_PORT_MAPPINGS//,/ }"
+
+        if [ -z "$HBASE_PORT_MAPPINGS" ]; then
+            return 0
+        fi
+
+        if [ -z "${HBASE_BACKGROUND_PIDS_FILE:-}" ]; then
+            echo >&2 "ERROR: no PIDs file"
+            exit 1
+        fi
+
         eval set -- "$HBASE_PORT_MAPPINGS"
         tcp-map-ports --pids-file "$HBASE_BACKGROUND_PIDS_FILE" "$@"
     )
