d721fd6a7cc493b9364b63c96d5a703c7ea3670b
C1310849 Diogenes da Silva Oliveira <c1310849@interno.bb.com.br>
2023-07-31 17:36:15 -0300

    try: nc

--- src/main/java/io/github/diogenes1oliveira/hbase2/HBaseContainer.java
+++ src/main/java/io/github/diogenes1oliveira/hbase2/HBaseContainer.java
@@ -73,8 +73,9 @@ public class HBaseContainer extends GenericContainer<HBaseContainer> {
         env.put(ENV_DOTENV_NAME, ENV_DOTENV_VALUE);
         withEnv(ENV_DOTENV_NAME, ENV_DOTENV_VALUE);
 
-        this.hostname = hostnameFunction.getHostname(this.getDockerClient());
+        this.hostname = hostnameFunction.getHostname(this);
         if(!"localhost".equals(this.hostname)) {
+            LOGGER.info("setting hostname {}=127.0.0.1", this.hostname);
             withExtraHost(hostname, "127.0.0.1");
         }
 
