de75d81c9d77572309c2344aca86a51d6f142efc
C1310849 Diogenes da Silva Oliveira <c1310849@interno.bb.com.br>
2023-07-31 15:40:26 -0300

    fix: maven get version

--- .dev/maven-get-version.sh
+++ .dev/maven-get-version.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
+set -eu
 
-grep -F '<version>' | tr -d '[:space:]' | tr '<>/' '|' | cut -d'|' -f 3
+version="$(grep -F '<version>' pom.xml | tr -d '[:space:]' | tr '<>/' '|' | cut -d'|' -f 3)"
+if [ -z "$version" ]; then
+    echo >&2 "ERROR: no version available"
+    exit 1
+fi
+
+if [ -t 1 ]; then
+    printf '%s\n' "$version"
+else
+    printf '%s' "$version"
+fi
